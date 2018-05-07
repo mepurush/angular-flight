93be4c4fcab4489a96bf094c6dea8c50e622be5a
PurushothamB <Purushotham.Bhakthan@ally.com>
2018-05-07 20:26:40 +0530

    added other modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,31 @@ import { AppBootstrapModule } from './app-bootstrap/app.bootstrap.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/Forms';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
+import { TrainComponent } from './train/train.component';
+import { BusComponent } from './bus/bus.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    TrainComponent,
+    BusComponent
   ],
   imports: [
     BrowserModule,
     AppBootstrapModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {path:'flights',component: SearchComponent},
+      {path:'trains', component:TrainComponent},
+      {path:'bus', component:BusComponent},
+      {path:'',redirectTo:'flights', pathMatch:'full'},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
